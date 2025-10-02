0aa542f839a6460c5f1cc2fc5d54f65d30816fc3
Rania Bhh <rania.hajhmida@gmail.com>
2025-10-02 23:05:13 +0100

    Premier commit : ajout du projet Symfony

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthorController extends AbstractController
+{
+    #[Route('/author/{name}', name: 'author_show')]
+    public function showAuthor(string $name): Response
+    {
+        return $this->render('author/show.html.twig', [
+            'name' => $name
+        ]);
+    }
+
+    #[Route('/authors', name: 'authors_list')]
+    public function listAuthors(): Response
+    {
+        $authors = [
+            [
+                'id' => 1,
+                'picture' => '/images/Victor-Hugo.jpg',
+                'username' => 'Victor Hugo',
+                'email' => 'victor.hugo@gmail.com',
+                'nb_books' => 100
+            ],
+            [
+                'id' => 2,
+                'picture' => '/images/william-shakespeare.jpg',
+                'username' => 'William Shakespeare',
+                'email' => 'william.shakespeare@gmail.com',
+                'nb_books' => 200
+            ],
+            [
+                'id' => 3,
+                'picture' => '/images/Taha_Hussein.jpg',
+                'username' => 'Taha Hussein',
+                'email' => 'taha.hussein@gmail.com',
+                'nb_books' => 300
+            ],
+        ];
+
+        $authorsCount = count($authors);
+        $totalBooks = array_sum(array_column($authors, 'nb_books'));
+
+        return $this->render('author/list.html.twig', [
+            'authors' => $authors,
+            'authorsCount' => $authorsCount,
+            'totalBooks' => $totalBooks
+        ]);
+    }
+
+    #[Route('/author/details/{id}', name: 'author_details')]
+    public function authorDetails(int $id): Response
+    {
+        $authors = [
+            1 => [
+                'id' => 1,
+                'picture' => '/images/Victor-Hugo.jpg',
+                'username' => 'Victor Hugo',
+                'email' => 'victor.hugo@gmail.com',
+                'nb_books' => 100
+            ],
+            2 => [
+                'id' => 2,
+                'picture' => '/images/william-shakespeare.jpg',
+                'username' => 'William Shakespeare',
+                'email' => 'william.shakespeare@gmail.com',
+                'nb_books' => 200
+            ],
+            3 => [
+                'id' => 3,
+                'picture' => '/images/Taha_Hussein.jpg',
+                'username' => 'Taha Hussein',
+                'email' => 'taha.hussein@gmail.com',
+                'nb_books' => 300
+            ],
+        ];
+
+        if (!isset($authors[$id])) {
+            throw $this->createNotFoundException('Auteur non trouvé');
+        }
+
+        $author = $authors[$id];
+
+        return $this->render('author/showAuthor.html.twig', [
+            'author' => $author
+        ]);
+    }
+}
